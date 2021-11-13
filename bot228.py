fcc3b891e3ecea8d7ce482a33dc35b0da4f594f6
rerete13 <58909158+rerete13@users.noreply.github.com>
2021-11-13 21:27:18 +0200

    Create bot228.py
    there is a bug, its essence is that when you call a callback function again it returns the value of the previous time

--- bot228.py
+++ bot228.py
@@ -1,10 +1,13 @@
-from pyowm import OWM
+from types import resolve_bases
+from bs4 import BeautifulSoup as BS
+import requests
 import telebot
-from telebot.types import Message
+from telebot.types import Message, Update
+from telebot import types
+from datetime import date, timedelta
 
-owm = OWM("e1b9dd31f0d4c906e723b1cb2dfc4fb3")
 
-token = '2094805370:AAFf1jRKeQa2ZpFvRDkHDv1wnHhOLgRtLFU'
+token = 'token'
 
 
 bot = telebot.TeleBot(token)
@@ -12,38 +15,285 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "hi, i am weather bot")
-    bot.send_message(message.chat.id, "enter your city")
+
+    bot.send_message(message.chat.id, "Привіт, введи своє місто")
 
 
 @bot.message_handler(content_types=['text'])
 def next(message):
 
+    markup = types.InlineKeyboardMarkup(row_width=1)
+
+    btn1 = types.InlineKeyboardButton(text='завтра', callback_data='1')
+    btn2 = types.InlineKeyboardButton(text='після завтра', callback_data='2')
+    btn3 = types.InlineKeyboardButton(
+        text='після після завтра', callback_data='3')
+
+    markup.add(btn1, btn2, btn3)
+
     if message.text.lower():
-        place = message.text
 
-        srch = owm.weather_manager()
+        city = message.text
 
-        let = srch.weather_at_place(place)
+        days = 0
 
-        show = let.weather
+        day = date.today()
+        day = day + timedelta(days)
 
-        tAll = show.temperature('celsius')
+        url1 = f'https://ua.sinoptik.ua/погода-{city}/{day}'
 
-        info = tAll['temp']
+        r = requests.get(url1)
 
-        bot.send_message(message.chat.id, 'Curent temperature in')
-        bot.send_message(message.chat.id, message.text)
-        bot.send_message(message.chat.id, 'is')
-        bot.send_message(message.chat.id, info)
+        html = BS(r.content, 'html.parser')
 
-        print(message.text)
+        all = []
 
-    else:
-        bot.send_message(message.chat.id, 'enter correct city')
+        all = html.find_all('td', class_='p5')
+
+        if all == []:
+            bot.send_message(
+                message.chat.id, 'Ви ввели назву міста не українською мовою, або назву неіснуючого міста')
+
+        temp = (all[2].text)
+        feelLike = (all[3].text)
+        wind = float(all[6].text)
+        rain = all[7].text
+
+        rep = ['-']
+
+        for i in rep:
+            if i in rain:
+                rain = rain.replace(i, str(0))
+
+        rainInfo = rain
+
+        rainInfo = float(rainInfo)
+
+        if rainInfo == '-':
+            rainInfoEnd = 'не передбачуються'
+
+        if rainInfo == 0:
+            rainInfoEnd = 'не передбачуються'
+
+        if rainInfo <= 20:
+            rainInfoEnd = 'не передбачуються'
+
+        if rainInfo >= 30:
+            rainInfoEnd = 'Можливі опади'
+
+        elif rainInfo >= 50:
+            rainInfoEnd = 'Присутні опади'
+
+        windInfo = wind
+
+        if windInfo <= 8:
+            windInfoEnd = 'слабкий'
+
+        if windInfo >= 8:
+            windInfoEnd = 'середній'
+
+        elif windInfo >= 15:
+            windInfoEnd = 'сильний'
+
+        bot.send_message(
+            message.chat.id, f'Погода сьогодні:\nТемпература: {temp} \nВідчувається як: {feelLike} \nВітер: {windInfoEnd}\nОпади: {rainInfoEnd}', reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback(call):
+
+        if call.message:
+
+            if call.data == '1':
+
+                if call.message.text.lower():
+
+                    city = message.text
+
+                    days = 1
+
+                    day = date.today()
+                    day = day + timedelta(days)
+
+                    print(day)
+
+                    url1 = f'https://ua.sinoptik.ua/погода-{city}/{day}'
+
+                    r = requests.get(url1)
+
+                    html = BS(r.content, 'html.parser')
+
+                    all = []
+
+                    all = html.find_all('td', class_='p5')
+
+                    temp = (all[2].text)
+                    feelLike = (all[3].text)
+                    wind = float(all[6].text)
+                    rain = all[7].text
+
+                    rainInfo = rain
+
+                    rainInfo = float(rainInfo)
+
+                    if rainInfo == '-':
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo == 0:
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo <= 20:
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo >= 30:
+                        rainInfoEnd = 'Можливі опади'
+
+                    elif rainInfo >= 50:
+                        rainInfoEnd = 'Присутні опади'
+
+                    windInfo = wind
+
+                    if windInfo <= 8:
+                        windInfoEnd = 'слабкий'
+
+                    if windInfo >= 8:
+                        windInfoEnd = 'середній'
+
+                    elif windInfo >= 15:
+                        windInfoEnd = 'сильний'
+
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                          text=f'Температура: {temp} \nВідчувається як: {feelLike} \nВітер: {windInfoEnd}\nОпади: {rainInfoEnd}')
+                return
+
+        if call.message:
+
+            if call.data == '2':
+
+                if call.message.text.lower():
+
+                    city = message.text
+
+                    days = 2
+
+                    day = date.today()
+                    day = day + timedelta(days)
+
+                    print(day)
+
+                    url1 = f'https://ua.sinoptik.ua/погода-{city}/{day}'
+
+                    r = requests.get(url1)
+
+                    html = BS(r.content, 'html.parser')
+
+                    all = []
+
+                    all = html.find_all('td', class_='p3')
+
+                    temp = (all[2].text)
+                    feelLike = (all[3].text)
+                    wind = float(all[6].text)
+                    rain = all[7].text
+
+                    rainInfo = rain
+
+                    rainInfo = float(rainInfo)
+
+                    if rainInfo == '-':
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo == 0:
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo <= 20:
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo >= 30:
+                        rainInfoEnd = 'Можливі опади'
+
+                    elif rainInfo >= 50:
+                        rainInfoEnd = 'Присутні опади'
+
+                    windInfo = wind
+
+                    if windInfo <= 8:
+                        windInfoEnd = 'слабкий'
+
+                    if windInfo >= 8:
+                        windInfoEnd = 'середній'
+
+                    elif windInfo >= 15:
+                        windInfoEnd = 'сильний'
+
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                          text=f'Температура: {temp} \nВідчувається як: {feelLike} \nВітер: {windInfoEnd}\nОпади: {rainInfoEnd}')
+
+                return
+
+        if call.message:
+
+            if call.data == '3':
+
+                if call.message.text.lower():
+
+                    city = message.text
+
+                    days = 3
+
+                    day = date.today()
+                    day = day + timedelta(days)
+
+                    print(day)
+
+                    url1 = f'https://ua.sinoptik.ua/погода-{city}/{day}'
+
+                    r = requests.get(url1)
+
+                    html = BS(r.content, 'html.parser')
+
+                    all = []
+
+                    all = html.find_all('td', class_='p3')
+
+                    temp = (all[2].text)
+                    feelLike = (all[3].text)
+                    wind = float(all[6].text)
+                    rain = all[7].text
+
+                    rainInfo = rain
+
+                    rainInfo = float(rainInfo)
+
+                    if rainInfo == '-':
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo == 0:
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo <= 20:
+                        rainInfoEnd = 'не передбачуються'
+
+                    if rainInfo >= 30:
+                        rainInfoEnd = 'Можливі опади'
+
+                    elif rainInfo >= 50:
+                        rainInfoEnd = 'Присутні опади'
+
+                    windInfo = wind
+
+                    if windInfo <= 8:
+                        windInfoEnd = 'слабкий'
+
+                    if windInfo >= 8:
+                        windInfoEnd = 'середній'
+
+                    elif windInfo >= 15:
+                        windInfoEnd = 'сильний'
+
+                    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id,
+                                          text=f'Температура: {temp} \nВідчувається як: {feelLike} \nВітер: {windInfoEnd}\nОпади: {rainInfoEnd}')
+
+                return
 
 
 bot.infinity_polling()
-
-
-input('end')
